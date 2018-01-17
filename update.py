7a52babd2265ffb33d0a7d0ad40eed28e52d56b2
pyro57 <kevinpyrosc@gmail.com>
2018-01-17 16:33:10 -0600

    added checking for root
    now that the install process changed I had to add a feature that checked to make sure it was run as root.

--- update.py
+++ update.py
@@ -1,51 +1,60 @@
-#/usr/bin/python
+#!/usr/bin/python
 #this is meant to run on Debian based distros, or anything that uses apt to manage packages
 
-from os import system
+import os
 import sys
+import subprocess
 
-#how to use the program
-usage = "python "sys.argv[0] "(list y)"
+#checks to see if it is run as root
+if os.geteuid() !=0:
+	print "I must be run as root!"
 
-#refreshes the apt cache
-def refresh():
-	print "updateing cache please wait..."
-	system("apt update")
-#lists all upgradeable packages
-def list():
-	system("apt list --upgradeable")
-	listan = raw_input("Upgrade all packages? ")
-	if "y" in listan:
-		upgrade()
-
-#upgrades the system		
-def upgrade():
-	system("apt upgrade -y")
-
-#refreshes the cache
-refresh()
-
-#checks to see if you gave it any instructions right off the bat
-if len(sys.argv) !=2:
-
-	#asks if you want to upgrade the packages
-        answer = raw_input("Upgrade all packages? ")
-
-	#checks to see if there is a y in your answer, if there is it upgrades
-        if "y" in answer:
-                upgrade()
-        #checks to see if the answer you gave was "list" if it is then it lists upgradable programs
-	elif answer == "list":
-                list()
-
-#checks to see if there you typed a y right after the name of the program, if so it upgrades with out asking
-elif "y" in sys.argv[1]:
-	upgrade()
-#checks to see if you typed list after the name of the program, if so it lists upgradeable packages
-elif sys.argv[1] == "list":
-	list()
-
-#tells you how to use the program
 else:
-	print usage
+
+	#how to use the program
+	usage = "sudo python update.py (list y)"
+
+
+	#refreshes the apt cache
+	def refresh():
+		print "updateing cache please wait..."
+		os.system("apt update")
+	#lists all upgradeable packages
+	def list():
+		os.system("apt list --upgradeable")
+		listan = raw_input("Upgrade all packages? ")
+		if "y" in listan:
+			upgrade()
+
+	#upgrades the system		
+	def upgrade():
+		system("apt upgrade -y")
+
+	#refreshes the cache
+	refresh()
+
+	#checks to see if you gave it any instructions right off the bat
+
+	if len(sys.argv) !=2:
+
+		#asks if you want to upgrade the packages
+        	answer = raw_input("Upgrade all packages? ")
+
+		#checks to see if there is a y in your answer, if there is it upgrades
+        	if "y" in answer:
+                	upgrade()
+        	#checks to see if the answer you gave was "list" if it is then it lists upgradable programs
+		elif answer == "list":
+                	list()
+
+	#checks to see if there you typed a y right after the name of the program, if so it upgrades with out asking
+	elif "y" in sys.argv[1]:
+		upgrade()
+	#checks to see if you typed list after the name of the program, if so it lists upgradeable packages
+	elif sys.argv[1] == "list":
+		list()
+
+	#tells you how to use the program
+	else:
+		print usage
 
